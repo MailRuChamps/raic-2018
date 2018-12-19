a1e939f574afc7683074873efe72d4e609333dd9
kruall <kru.bash.all@gmail.com>
2018-12-20 00:39:52 +0500

    move qs

--- QUICKSTARTS/f#/MyStrategy.fs
+++ QUICKSTARTS/f#/MyStrategy.fs
@@ -12,19 +12,30 @@ type StrategyData =
 
 type ActData = Robot * Rules * Game * StrategyData
 
-
 module MyStrategy = 
     let private EPS = 0.1
-    let private max_speed = ROBOT_MAX_GROUND_SPEED
-    let private max_speed32 = float32 ROBOT_MAX_GROUND_SPEED
-    let private max_jump = ROBOT_MAX_JUMP_SPEED
 
     let private vector2 x y = Vector2 (x, y) 
 
+    let private posFromRobot (m : Robot) = Vector3 (float32 m.x, float32 m.y, float32 m.z) 
+    let private velFromRobot (m : Robot) = Vector3 (float32 m.velocity_x, float32 m.velocity_y, float32 m.velocity_z) 
+    
+    let private posFromBall (m : Ball) = Vector3 (float32 m.x, float32 m.y, float32 m.z) 
+    let private velFromBall (m : Ball) = Vector3 (float32 m.velocity_x, float32 m.velocity_y, float32 m.velocity_z) 
+    
+    let private pos2FromRobot (m : Robot) = Vector2 (float32 m.x, float32 m.z) 
+    let private vel2FromRobot (m : Robot) = Vector2 (float32 m.velocity_x, float32 m.velocity_z) 
 
-    let getJumpSpeed (robot : Robot) (ball : Ball) (dist  : float) = 
-        let need_jump = dist < (BALL_RADIUS + ROBOT_MAX_RADIUS) && robot.y < ball.y
-        if need_jump then max_jump else 0.   
+    let private pos2FromBall (m : Ball) = Vector2 (float32 m.x, float32 m.z) 
+    let private vel2FromBall (m : Ball) = Vector2 (float32 m.velocity_x, float32 m.velocity_z) 
+
+
+    let private getJumpSpeed (dist_for_jump : float) (jump_speed : float) (robot : Robot) (ball : Ball) (dist : float) : float = 
+        let need_jump = dist < dist_for_jump && robot.y < ball.y
+        if need_jump then jump_speed else 0.0
+
+    let private isNormalSpeed max_speed speed = 
+        0.5 * max_speed < speed && speed < max_speed
 
 
     let protectAct (args : ActData) : Action =
@@ -41,43 +52,68 @@ module MyStrategy =
             else
                 vector2 (float32 x) (float32 target_pos_z)
 
-        let target_velocity = (interception - Robot.position2 me) * max_speed32
+        let me_pos2 = pos2FromRobot me
+        let max_speed32 = float32 rules.ROBOT_MAX_GROUND_SPEED
+        let target_velocity = (interception - me_pos2) * max_speed32
 
-        let delta = (Ball.position ball) - (Robot.position me)
+        let delta = (pos2FromBall ball) - me_pos2
 
-        let jump_speed = delta.Length() |> float |> getJumpSpeed me ball
+        let dist_for_jump = rules.ROBOT_RADIUS + rules.BALL_RADIUS
+        let max_jump_speed = rules.ROBOT_MAX_JUMP_SPEED
+        let jump_speed = 
+            delta.Length() 
+            |> float 
+            |> getJumpSpeed dist_for_jump max_jump_speed me ball
 
-        Action.fromVelocity2 target_velocity jump_speed true
-    
+        {
+            target_velocity_x = float target_velocity.X
+            target_velocity_y = 0.0
+            target_velocity_z = float target_velocity.Y
+            jump_speed = jump_speed
+            use_nitro = true
+        }
 
-    let isNormalSpeed speed = 
-        0.5 * max_speed < speed && speed < max_speed
 
+    let attackEntryPoint (me, rules, game, _) : Action option = 
+        let delta = (posFromBall game.ball) - (posFromRobot me)
 
-    let attackEntryPoint (me, arena, ball) : Action option = 
-        let delta = (Ball.position ball) - (Robot.position me)
-        let jump_speed = delta.Length() |> float |> getJumpSpeed me ball
+        let dist_for_jump = rules.ROBOT_RADIUS + rules.BALL_RADIUS
+        let max_jump_speed = rules.ROBOT_MAX_JUMP_SPEED
+        let jump_speed = 
+            delta.Length() 
+            |> float 
+            |> getJumpSpeed dist_for_jump max_jump_speed me game.ball
         
-        let me_pos2 = Robot.position2 me
+        let me_pos2 = pos2FromRobot me
+
+        let max_speed = rules.ROBOT_MAX_GROUND_SPEED
+        let isPossibleSpeed : float -> bool = isNormalSpeed max_speed
 
         let tryAttack (pos2 : Vector2, time : float32) = 
             let delta2 = pos2 - me_pos2
             let need_speed = delta.Length() / time
-            let velocity2 = delta2 / time
-            match isNormalSpeed (float need_speed) with
-            | true -> Some <| Action.fromVelocity2 velocity2 jump_speed true
+            let target_velocity = delta2 / time
+            match isPossibleSpeed (float need_speed) with
+            | true -> 
+                Some {                
+                    target_velocity_x = float target_velocity.X
+                    target_velocity_y = 0.0
+                    target_velocity_z = float target_velocity.Y
+                    jump_speed = jump_speed
+                    use_nitro = true
+                }
             | false -> None      
 
-        let ball_pos2 = Ball.position2 ball
-        let ball_pos2_delta = Ball.velocity2 ball * 0.1f
+        let ball_pos2 = pos2FromBall game.ball
+        let ball_pos2_delta = vel2FromBall game.ball * 0.1f
         let ballPosAfterTicksWithTime ticks  = 
             let new_pos2 = ball_pos2 + ball_pos2_delta * float32 ticks
             let new_time = 0.1f * float32 ticks
             (new_pos2, new_time)
 
         let ballInArena (pos2 : Vector2, _) = 
-            abs(float pos2.X) < arena.width / 2. 
-            && abs(float pos2.Y) < arena.depth / 2.
+            abs(float pos2.X) < rules.arena.width / 2. 
+            && abs(float pos2.Y) < rules.arena.depth / 2.
 
         Array.init 100 ballPosAfterTicksWithTime
         |> Array.takeWhile ballInArena
@@ -85,8 +121,7 @@ module MyStrategy =
 
 
     let attackAct (args : ActData) : Action =
-        let me, rules, game, _ = args
-        match attackEntryPoint (me, rules.arena, game.ball) with
+        match attackEntryPoint args with
         | Some x -> x
         | None -> protectAct args
     
@@ -108,9 +143,9 @@ module MyStrategy =
         | KnownAttackBot(tick, _) when tick = cur_tick -> data
         | otherwise ->
             game.robots
-            |> Array.filter Robot.is_teammate
-            |> Array.maxBy Robot.z
-            |> Robot.id 
+            |> Array.filter (fun x -> x.is_teammate)
+            |> Array.maxBy (fun x -> x.z)
+            |> (fun x -> x.id)
             |> tuple2 cur_tick
             |> KnownAttackBot
 
