de8941ff399d3399f39e41b631ee782a14bc0cda
Vasilev Pyotr <vahpetr@gmail.com>
2018-12-24 01:50:11 +0500

    Add default values in Arena.js

--- packages/javascript/model/Arena.js
+++ packages/javascript/model/Arena.js
@@ -1,16 +1,16 @@
 class Arena {
   constructor() {
-    this.width = 0;
-    this.height = 0;
-    this.depth = 0;
-    this.bottom_radius = 0;
-    this.top_radius = 0;
-    this.corner_radius = 0;
-    this.goal_top_radius = 0;
-    this.goal_width = 0;
-    this.goal_height = 0;
-    this.goal_depth = 0;
-    this.goal_side_radius = 0;
+    this.width = 60;
+    this.height = 20;
+    this.depth = 80;
+    this.bottom_radius = 3;
+    this.top_radius = 7;
+    this.corner_radius = 13;
+    this.goal_top_radius = 3;
+    this.goal_width = 30;
+    this.goal_height = 10;
+    this.goal_depth = 10;
+    this.goal_side_radius = 1;
   }
 }
 module.exports = Arena;
